8049cd831bade318fd43481cea64687d49440e80
frejala <fa.rejala03@gmail.com>
2024-07-27 18:08:10 -0400

    feat: Add favicon and font styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'o-blue': '#49DEFF',
+        'o-yellow': '#FFC100',
+        'o-black': '#121214',
+      },
+      fontFamily: {
+        'space-mono': ['Space Mono', 'monospace'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      textStroke: {
+        'white-1': '1px white',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.text-stroke-white-1': {
+          '-webkit-text-stroke': '1px white',
+          'text-stroke': '1px white',
+        },
+        '.text-stroke-none': {
+          '-webkit-text-stroke': '0px white',
+          'text-stroke': '0px white',
+        },
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
 };
