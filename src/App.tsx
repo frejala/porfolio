420ef089ee0ee6c8ae16ba3e93212420501e7c74
frejala <fa.rejala03@gmail.com>
2024-07-27 11:19:15 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-xl text-blue-500">
+        New Portfolio
+      </h1>
+    </div>
+  );
+}
+
+export default App;
